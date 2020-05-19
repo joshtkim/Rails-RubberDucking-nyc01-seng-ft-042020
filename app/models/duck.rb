1e2d598ff3cbd5fa8c0ee6ea0a99aefc5af6b103
Joshua <jykim92@gmail.com>
2020-05-19 16:15:41 -0400

    Done.

--- app/models/duck.rb
+++ app/models/duck.rb
@@ -1,2 +1,5 @@
 class Duck < ApplicationRecord
+    belongs_to :student
+
+    validates :name, :description, presence: {message: "fields cannot be blank"}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-10.times do
+
+
+puts "Seeding the data"
+Student.destroy_all
+Duck.destroy_all
+
+5.times do
   Student.create(name: Faker::Name.name, mod: rand(1..5))
 end
 
-5.times do
-  Duck.create(name: Faker::Creature::Dog.meme_phrase, description: Faker::Creature::Dog.breed)
+30.times do
+  Duck.create(name: Faker::Creature::Dog.meme_phrase, description: Faker::Creature::Dog.breed, student_id: Student.all.sample.id)
 end
